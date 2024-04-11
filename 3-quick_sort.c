2e43d0a4c5485108ee02db8c08042fca139ecdb3
psychohight <7298@holbertonstudents.com>
2024-04-11 19:12:22 +0200

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,11 +1,77 @@
 #include "sort.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * swap - Echange les valeurs de deux éléments d'un tableau.
+ * @a: le premier élément
+ * @b: le second élément
+ * Return: rien.
+ */
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+/**
+ * partition - Partitionne le tableau
+ * @array: tableau à travailler
+ * @low: l'indice de départ
+ * @high: l'indice de fin
+ * @size: la taille du tableau
+ * Return: l'indice du pivot
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = (low - 1);
+    
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+            if (i != j)
+                print_array(array, size);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    
+    if (i + 1 != high)
+        print_array(array, size);
+    
+    return (i + 1);
+}
+
+/**
+ * quick_sort_rec - Fonction récursive pour appliquer le tri rapide.
+ * @array: le tableau
+ * @low: l'indice de départ
+ * @high: l'indice de fin
+ * @size: taille du tableau
+ * Return: rien.
+ */
+void quick_sort_rec(int *array, int low, int high, size_t size)
+{
+    if (low < high)
+    {
+        int i = partition(array, low, high, size);
+        quick_sort_rec(array, low, i - 1, size);
+        quick_sort_rec(array, i + 1, high, size);
+    }
+}
+
+/**
+ * quick_sort - Trie un tableau d'entiers ordre croissant.
+ * @array: le tableau
+ * @size: taille du tableau
+ * Return: rien.
  */
 void quick_sort(int *array, size_t size)
 {
-    
+    if (!array || size < 2)
+        return;
+
+    quick_sort_rec(array, 0, size - 1, size);
 }
